9a67ac98d3a415a80dca6b7b651663b3c964676b
Frank Förster <frank.foerster@ime.fraunhofer.de>
2018-04-20 11:08:24 +0200

    Add a md5 checksum for the uploads

--- BioInf-ServiceWS/lib/BioInf/ServiceWS.pm
+++ BioInf-ServiceWS/lib/BioInf/ServiceWS.pm
@@ -1,5 +1,6 @@
 package BioInf::ServiceWS;
 use Dancer2;
+use Digest::MD5;
 
 our $VERSION = '0.1';
 
@@ -16,7 +17,6 @@ post '/arts' => sub {
     my $all_uploads = request->uploads;
     
     use Data::Dumper;
-
     print Dumper({request->params});
     print Dumper($all_uploads);
 
@@ -25,8 +25,27 @@ post '/arts' => sub {
     my $tempname = request->upload('upload')->tempname;
     my $email    = request->param('email');
     my $jobid    = 42;
-    
-    template upload => { filename => $filename, size => $size, email => $email, jobid => $jobid, analysisname => "ARTS" };
+
+    # calculate a checksum for the uploaded file
+    my $checksum = generate_md5($tempname);
+
+    template upload => { filename => $filename, size => $size, email => $email, jobid => $jobid, analysisname => "ARTS", checksum => $checksum };
 };
+
+sub generate_md5
+{
+    my ($filename) = @_;
+
+    my $ctx = Digest::MD5->new;
+
+    open(FH, "<", $filename) || die "Unable to open file: '$filename': $!\n";
+    while(<FH>)
+    {
+	$ctx->add($_);
+    }
+    close(FH) || die "Unable to close file: '$filename': $!\n";
+
+    return $ctx->hexdigest;
+}
 
 true;
