1d86831b7ccc2f809be0b9f9e55fa82354e0ff20
Frank Förster <frank.foerster@ime.fraunhofer.de>
2018-05-23 10:51:40 +0200

    Collect information about categories and return the list

--- BioInf-ServiceWS/lib/BioInf/ServiceWS/API.pm
+++ BioInf-ServiceWS/lib/BioInf/ServiceWS/API.pm
@@ -20,7 +20,29 @@ get '/openproject_categories' => sub {
     my $request = GET $url.'/api/v3/projects';
     $request->authorization_basic('apikey', $apikey);
     my $response = $ua->request($request);
-    $data = decode_json($response->decoded_content());
+    my $dat = decode_json($response->decoded_content());
+
+    # go through projects and request all categories:
+    foreach my $project (@{$dat->{_embedded}{elements}})
+    {
+	my $name = $project->{identifier};
+	my $category_uri = $base_uri.$project->{_links}{categories}{href};
+
+	my $request = GET $category_uri;
+	$request->authorization_basic('apikey', $apikey);
+	my $response = $ua->request($request);
+
+	my $categories = decode_json($response->decoded_content());
+
+	foreach my $cat (@{$categories->{_embedded}{elements}})
+	{
+	    next unless ($cat->{_type} eq "Category");
+	    my $cat_link = $cat->{_links}{self}{href}
+	    my $cat_title = $cat->{_links}{self}{title}
+
+	    push(@{$data->{$name}}, { link => $cat_link, title => $cat_title });
+	}
+    }
 
     return $data;
 };
