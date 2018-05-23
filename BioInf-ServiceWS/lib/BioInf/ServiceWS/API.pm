9f02f5433590d3841a47fb5c22af3789f1099f31
Frank Förster <frank.foerster@ime.fraunhofer.de>
2018-05-23 10:36:15 +0200

    Retrieve list of projects

--- BioInf-ServiceWS/lib/BioInf/ServiceWS/API.pm
+++ BioInf-ServiceWS/lib/BioInf/ServiceWS/API.pm
@@ -1,10 +1,26 @@
 package BioInf::ServiceWS::API;
 use Dancer2;
 
+use LWP::UserAgent;
+use HTTP::Request::Common;
+
 set serializer => 'JSON';
 
 get '/openproject_categories' => sub {
     my $data = {};
+
+    my $url    = '';
+    my $apikey = '';
+
+    my $u = URI->new($url);
+    my $base_uri=$u->scheme."://".$u->host_port;
+
+    # create the request to optain all accessable projects
+    my $ua = LWP::UserAgent->new();
+    my $request = GET $url.'/api/v3/projects';
+    $request->authorization_basic('apikey', $apikey);
+    my $response = $ua->request($request);
+    $data = decode_json($response->decoded_content());
 
     return $data;
 };
